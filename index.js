8688836f3dbd162749b3092cfefb05ab520e0c58
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2025-04-14 00:26:40 +0530

    Minimal working blockchain 2 -> minor index.js file fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { app, p2pServer } = require('./app')
+const { app, initP2PServer } = require('./app')
 const config = require('./utils/config')
 
 app.listen(config.HTTP_PORT, () => {
     console.log(`Listening http on port: ${config.HTTP_PORT} `)
 })
 
-p2pServer()
+initP2PServer(config.P2P_PORT)
